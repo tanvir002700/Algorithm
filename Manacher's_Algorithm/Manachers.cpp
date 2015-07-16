0b550b046c791cb46b9e4507c04d8efd5621b25e
Tanvir Hasan Anick <tanvir002700@gmail.com>
2015-07-16 13:06:26 +0600

    update

--- Manacher's_Algorithm/Manachers.cpp
+++ Manacher's_Algorithm/Manachers.cpp
@@ -5,42 +5,35 @@ char str[1000005];
 void prepare_string()
 {
     int i;
-    t = "^#";
-    for(i = 0; i < s.size(); i++)
-        t += s[i], t += "#";
-    t += "$";
+    t="^#";
+    for(i=0;i<s.size();i++)
+        t+=s[i],t+="#";
+    t+="$";
 }
 
 int manacher()
 {
     prepare_string();
-
-    int P[t.size()], c = 0, r = 0, i, i_mirror, n = t.size() - 1;
-
-    for(i = 1; i < n; i++)
+    int P[t.size()],c=0,r=0,i,i_mirror,n=t.size()-1;
+    for(i=1;i<n;i++)
     {
-        i_mirror = (2 * c) - i;
-
-        P[i] = r > i? min(r - i, P[i_mirror]) : 0;
-
-        while(t[i + 1 + P[i]] == t[i - 1 - P[i]])
-            P[i]++;
-
-        if(i + P[i] > r)
+        i_mirror=(2*c)-i;
+        P[i]=r>i?min(r-i,P[i_mirror]):0;
+        while(t[i+1+P[i]]==t[i-1-P[i]]) P[i]++;
+        if(i+P[i]>r)
         {
-            c = i;
-            r = i + P[i];
+            c=i;
+            r=i+P[i];
         }
     }
-    return *max_element(P + 1, P + n);
+    return *max_element(P+1,P+n);
 }
-
 int main()
 {
-    int kase = 1;
-    while(scanf(" %s", str) && str[0] != 'E')
+    int kase=1;
+    while(scanf(" %s", str)&&str[0]!= 'E')
     {
-        s = str;
+        s=str;
         printf("Case %d: %d\n", kase++, manacher());
     }
     return 0;
